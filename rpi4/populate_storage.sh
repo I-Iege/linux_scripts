9f12344d0dad3153e4de1597411104b30b133386
Máté Gál <gal.mateo@gmail.com>
2025-03-24 01:58:00 +0100

    Update populate_storage.sh

--- rpi4/populate_storage.sh
+++ rpi4/populate_storage.sh
@@ -1,24 +1,34 @@
 populate_dev() {
+
     local url="http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-aarch64-latest.tar.gz"
     local tarball="ArchLinuxARM-rpi-aarch64-latest.tar.gz"
     local root_mount="/mnt/root"
     local boot_mount="/mnt/boot"
     
     echo "Downloading root filesystem..."
-    wget "$url" || { echo "Download failed"; return 1; }
+    if [ ! -f "$(basename "$url")" ]; then
+        wget "$url" || { echo "Download failed"; return 1; }
+    else
+        echo "File $(basename "$url") already exists, skipping download."
+    fi
     
     echo "Extracting root filesystem..."
     sudo bsdtar -xpf "$tarball" -C "$root_mount" || { echo "Extraction failed"; return 1; }
+    sudo mv $root_mount/boot/* $boot_mount
     
     echo "Syncing changes..."
     sync
     
-    echo "Moving boot files..."
-    sudo cp -r "$root_mount/boot"/* "$boot_mount" || { echo "Failed to move boot files"; return 1; }
+    echo "Patching the Boot Config Files..."
+    cd $boot_mount
+    sudo sed -i 's/booti ${kernel_addr_r} ${ramdisk_addr_r}:${filesize} ${fdt_addr_r};/booti ${kernel_addr_r} ${ramdisk_addr_r}:${filesize} ${fdt_addr};/' boot.txt
+    sudo sed -i 's/booti ${kernel_addr_r} - ${fdt_addr_r};/booti ${kernel_addr_r} - ${fdt_addr};/' boot.txt
+    sudo ./mkscr
     
-    echo "Cleanup..."
-    sudo rm "$tarball"
-    
+    echo "Unmounting drives..."
+    sudo umount -l $root_mount
+    sudo umount -l $boot_mount
+
     echo "Done."
 }
 
